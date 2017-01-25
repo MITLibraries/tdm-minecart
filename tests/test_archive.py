7825bfc57946ac53281d6625c5bda6e5fae2e5a1
Mike Graves <mgraves@mit.edu>
2017-01-25 13:59:30 -0500

    Add archiver
    Adds support creating Zip archives.

---
+++ tests/test_archive.py
@@ -0,0 +1,31 @@
+import tempfile
+import zipfile
+
+import pytest
+
+from minecart.archive import archive, Zip
+
+
+@pytest.yield_fixture
+def thesis():
+    with tempfile.NamedTemporaryFile() as t:
+        t.write(b'foobar')
+        t.flush()
+        yield t.name
+
+
+def test_zip_writes_file_to_archive(thesis):
+    with tempfile.TemporaryFile() as fp:
+        arx = Zip(fp)
+        arx.write(thesis, 'test.txt')
+        arx.close()
+        with zipfile.ZipFile(fp) as zf:
+            assert b'foobar' == zf.read('test.txt')
+
+
+def test_archive_returns_writeable_archive(thesis):
+    with tempfile.NamedTemporaryFile() as fp:
+        with archive(fp.name) as arx:
+            arx.write(thesis, 'test.txt')
+        with zipfile.ZipFile(fp) as zf:
+            assert 'test.txt' in zf.namelist()
